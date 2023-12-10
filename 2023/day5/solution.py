33102641642efb6e1dc9025a9f01e5922b561a65
Lui Crowie <lui.crowie@crowzfx.co.uk>
2023-12-10 00:26:20 +0000

    added in day 5 and 6 placeholders

---
+++ 2023/day5/solution.py
@@ -0,0 +1,12 @@
+"""
+# Author: Lui Crowie
+# Task: https://adventofcode.com/2023/day/5
+"""
+# Get input from file
+with open("2023/day5/test.txt", "r") as file:
+  almanac = file.readlines()
+
+"""
+Part 1
+"""
+print(almanac)
---
+++ 2023/day6/solution.py
@@ -0,0 +1,12 @@
+"""
+# Author: Lui Crowie
+# Task: https://adventofcode.com/2023/day/6
+"""
+# Get input from file
+with open("2023/day6/test.txt", "r") as file:
+  documents = file.readlines()
+
+"""
+Part 1
+"""
+print("cheese")
