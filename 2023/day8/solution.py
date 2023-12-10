64ac46e95f6182f12123c034978c16d808a4de1b
Lui Crowie <lui.crowie@crowzfx.co.uk>
2023-12-10 00:26:58 +0000

    day 8 placeholder

---
+++ 2023/day8/solution.py
@@ -0,0 +1,12 @@
+"""
+# Author: Lui Crowie
+# Task: https://adventofcode.com/2023/day/8
+"""
+# Get input from file
+with open("2023/day8/input.txt", "r") as file:
+  network = file.readlines()
+
+"""
+Part 1
+"""
+print(network)
